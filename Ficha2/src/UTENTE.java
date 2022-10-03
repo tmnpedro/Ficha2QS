9bd6b874d4343112770b0569d1017d89fa4dab9a
HP <HP@10.180.2.53>
2022-10-03 14:47:02 +0100

    versao 1 parametros e construtor

---
+++ Ficha2/src/UTENTE.java
@@ -0,0 +1,17 @@
+
+public class UTENTE {
+	
+	
+	private String nome;
+	private int idade;
+	private float altura;
+	private int peso;
+	
+	public UTENTE(String nome, int idade, float altura, int peso){
+		this.nome = nome;
+		this.idade = idade;
+		this.altura = altura;
+		this.peso = peso;
+		
+	}
+}
