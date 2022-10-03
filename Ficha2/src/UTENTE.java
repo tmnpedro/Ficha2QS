3372d550eb3c4bcbdbd10a3cbf8b4f627894b182
HP <HP@10.180.2.53>
2022-10-03 15:21:53 +0100

    Versao 4 - metodo verificar

--- Ficha2/src/UTENTE.java
+++ Ficha2/src/UTENTE.java
@@ -27,6 +27,60 @@ public class UTENTE {
 			float imc = peso/(altura*altura);
 			return imc;
 		}
+		public String grauObesidade(float imc) {
+			String grau;
+			if(imc <=19 ) { grau = "Abaixo do peso normal";}
+			else if(imc <=25) {grau = "Peso normal";}
+			else if(imc <=30) {grau = "Excesso de peso";}
+			else {grau = "Obeso";}
+			return grau;
+		}
+		
+		//pergunta E
+		/**
+		 * Este método conforme o calculo do imc informa se é preciso alterar o peso ou não 
+		 * @param peso
+		 * @param altura
+		 */
+		public void verificar(int peso, float altura) {
+			float imc = calculaIMC(peso, altura);//calcula imc do utente.
+			boolean mudou = false;
+			
+			//caso imc seja menor que 19 calcula o peso que seria ideal.
+			while(imc <= 19) {
+				peso = peso + 1;
+				imc = calculaIMC(peso, altura);
+				mudou = true;//Muda o bool para saber que não está no peso ideal.
+			}
+			
+			//caso imc seja maior que 25 calcula o peso que seria ideal.
+			while(imc > 25) {
+				peso = peso -1;
+				imc = calculaIMC(peso, altura);
+				mudou = true;//Muda o bool para saber que não está no peso ideal.
+			}
+			
+			//Caso não tenha entrado em nenhum dos whiles anteriores quer dizer que está no peso ideal.
+			if(mudou == true) {
+			System.out.println("O peso a alcançar é de " + peso);
+			}
+			else {
+				System.out.println("Está no peso ideal, deve manter-se entre:");
+				//Calcula peso mínimo.
+				while(imc > 19) {
+					peso = peso - 1;
+					imc = calculaIMC(peso, altura);
+				}
+				System.out.println("Peso mínimo é : " + peso);
+				
+				//Cálcula peso máximo.
+				while(imc <= 25) {
+					peso = peso + 1;
+					imc = calculaIMC(peso, altura);
+				}
+				System.out.println("Peso máximo é: " + peso);
+			}
+		}
 		
 
 	public String getNome() {
