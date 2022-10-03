b4850de1ac1c2e407f4c469ac37611af32d1a83d
HP <HP@10.180.2.53>
2022-10-03 14:50:22 +0100

    Versão 2 - get e setters

--- Ficha2/src/UTENTE.java
+++ Ficha2/src/UTENTE.java
@@ -14,4 +14,36 @@ public class UTENTE {
 		this.peso = peso;
 		
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public float getAltura() {
+		return altura;
+	}
+
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
 }
